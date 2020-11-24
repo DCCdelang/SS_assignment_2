87b0bae18989d0d12522d9760a9bda03d00e3084
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-11-24 21:53:54 +0100

    Update des_simulation.py

--- des_simulation.py
+++ des_simulation.py
@@ -5,23 +5,21 @@ import simpy
 
 RANDOM_SEED = 420
 # NEW_CUSTOMERS = 5  # Total number of customers
-INTERVAL_CUSTOMERS = 10.0  # Generate new customers roughly every x seconds
+LAMBDA = 3.0  # Generate new customers roughly every x seconds
 MIN_PATIENCE = 1  # Min. customer patience
 MAX_PATIENCE = 10  # Max. customer patience
 
 
-def generate_customers(env, interval, counter):
+def generate_customers(env, arrival_rate, counter):
     """generates customers randomly"""
-    i = 0
-    waiting_times = []
+    id = 0
     while True:
-        i += 1
-        c = customer(env, 'Customer%02d' % i, counter, time_in_bank=12.0)
+        id += 1
+        c = customer(env, 'Customer%02d' % id, counter, time_in_bank=12.0)
         env.process(c)
-        t = random.expovariate(1.0 / interval)
-        waiting_times.append(t)
-        yield env.timeout(t)
-    print('avg waiting time:', np.mean(waiting_times))
+        mu = random.expovariate(1.0/arrival_rate)
+        wait_time.append(mu)
+        yield env.timeout(mu)
 
     
 
@@ -32,22 +30,35 @@ def customer(env, name, counter, time_in_bank):
     print('%7.4f %s: Here I am' % (arrive, name))
 
     with counter.request() as req:
-        patience = random.uniform(MIN_PATIENCE, MAX_PATIENCE)
-        # Wait for the counter or abort at the end of our tether
-        results = yield req | env.timeout(patience)
+        # patience = random.uniform(MIN_PATIENCE, MAX_PATIENCE)
+        # # Wait for the counter or abort at the end of our tether
+        # results = yield req | env.timeout(patience)
 
         wait = env.now - arrive
 
-        if req in results:
-            # We got to the counter
-            print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        
+        print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        tib = random.expovariate(1.0 / time_in_bank)
+        yield env.timeout(tib)
+        print('%7.4f %s: Finished' % (env.now, name))
 
-            tib = random.expovariate(1.0 / time_in_bank)
-            yield env.timeout(tib)
-            print('%7.4f %s: Finished' % (env.now, name))
-
-        else:
-            # We reneged
-            print('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
+        # else:
+        #     # We reneged
+        #     print('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
 
     return wait
+
+        
+
+    
+# Setup and start the simulation with n = 1, 2 and 4. 
+servers = [1,2,4]
+wait_time = [] # list to hold the time until 
+for n in servers:
+    print('servers:', n)
+
+    random.seed(RANDOM_SEED)
+    env = simpy.Environment()
+    counter = simpy.Resource(env, capacity=n)
+    env.process(generate_customers(env, LAMBDA, counter))
+    env.run(until=1000)
