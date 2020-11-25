d946b4e9e1245e90e76482c0202492f7ae59f826
DCCdelang <dccdelang@gmail.com>
2020-11-25 16:19:48 +0100

    additions dante

---
+++ data_analysis.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+"""
+Nu iets met seaborn zoals een boxplot met 
+"""
--- des_sim_dante.py
+++ des_sim_dante.py
@@ -4,19 +4,17 @@ DES program to simulate M/M/n queue's
 
 import random
 import simpy
-import csv
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import pandas as pd
+import pandas as pd
 import numpy as np
 
 
-LAMBDA = 3.0  # Generate new customers roughly every x seconds
-MU = 10 # Total waiting time???? 
+LAMBDA = 10.0  # Generate new customers roughly every lambda seconds
+MU = 9 # Expected waiting time per customer in seconds
+N_CUSTOMERS = 1000
 
 class System(object):
-    """Class for whole server queue system"""
-    def __init__(self, env, n_server, mu, lambd):
+    """Class for one server queue system"""
+    def __init__(self, env, n_server, n_cust, mu, lambd):
         self.env = env
         self.server = simpy.Resource(env, capacity=n_server)
         self.waittime = 0
@@ -24,11 +22,11 @@ class System(object):
         self.total_cust = 0
         self.mu = mu
         self.lambd = lambd
+        self.n_cust = n_cust
 
 def customer(env, system):
     """Customer arrives, is served and leaves."""
     arrive = env.now
-
     with system.server.request() as req:
         yield req 
 
@@ -36,47 +34,42 @@ def customer(env, system):
         system.waittime += wait
 
         # Time in system
-        tis = random.expovariate(1.0 / system.mu)
+        tis = random.expovariate(1/system.mu)
         yield env.timeout(tis)
         
         sojourn = env.now - arrive
         system.sojourn += sojourn
 
 def setup(env,system):
-    while True:
+    """Adding customers for one system simulations"""
+    for c in range(system.n_cust):
         system.total_cust += 1
         env.process(customer(env, system)) # Add customer to process
-        t = random.expovariate(1.0 / system.lambd)
+        t = random.expovariate(1/system.lambd)
         yield env.timeout(t)
 
 N_servers = [1,2,4]
-N_sim = 10
+N_sim = 100
+data_list = [[],[]]
 
-cust_list = [[],[],[]]
-wait_list = [[],[],[]]
-mean_wait_list = [[],[],[]]
-
-# data = open('data.csv','w')
+# For loop to run all simulations
 for i in range(len(N_servers)):
     for n_sim in range(N_sim):
+
         # Setup and start the simulation
         RANDOM_SEED = random.randint(1,700)
         random.seed(RANDOM_SEED)
         env = simpy.Environment()
-        system = System(env, N_servers[i], MU, LAMBDA/N_servers[i])
+        system = System(env, N_servers[i], N_CUSTOMERS, MU, LAMBDA/N_servers[i])
 
         env.process(setup(env, system))
-        env.run(until=1000)
-        # data.write(str(system.waittime) + "\n")
-        cust_list[i].append(system.total_cust)
-        wait_list[i].append(system.waittime)
-        mean_wait_list[i].append(system.waittime/system.total_cust)
+        env.run()
 
-print("N_cust:",cust_list)
-print("Total wait:",wait_list)
-print("Mean wait:", mean_wait_list)
+        data_list[0].append(i)
+        data_list[1].append(system.waittime/system.total_cust)
 
-for i in range(len(N_servers)):
-    print(np.mean(cust_list[i]))
-    print(np.mean(wait_list[i]))
-    print(np.mean(mean_wait_list[i]))
+data = {"Server": data_list[0], "Mean Wait": data_list[1]}
+df = pd.DataFrame(data)
+df.to_csv("data.csv")
+
+print("Done!")
