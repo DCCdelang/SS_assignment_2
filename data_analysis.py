860ff964a830d46434aff838d402f1b55543457f
DCCdelang <dccdelang@gmail.com>
2020-11-27 08:41:16 +0100

    Additions dante

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,36 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("data.csv")
+#%%
+# Bocplot function for all servers combined 
+def boxplot_wait():
+    """Create boxplot of mean time, according to csv"""
+    df = pd.read_csv("data.csv")
+    sns.boxplot(x = df["Server"], y = df["Mean Wait"])
+    plt.show()
+boxplot_wait()
 
-"""
-Nu iets met seaborn zoals een boxplot met 
-"""
+#%%
+# Give three plots of the distributions found by the mean waiting time for 
+# all simulations per server
+for n in range(3):
+    df2 = pd.read_csv("data.csv")
+    serverdata = df2.loc[df2["Server"] == n, "Mean Wait"]
+    sns.distplot(serverdata, bins= 20, rug=True)
+    plt.show()
+
+# %%
+# Gives three plots of the waiting time per customer for one simulation
+for i in range(3):
+    df3 = pd.read_csv("test_server_"+str(i)+".csv")
+    sns.lineplot(data=df3["Waiting"])
+    plt.show()
+
+# %%
+# Plot to determine when the steady state starts
+for n in range(3):
+    df4 = pd.read_csv("waiting_pc.csv")
+    waitingdata = df4.loc[df4["Server"] == n, "Waiting pc"]
+    sns.lineplot(data=waitingdata)
+    plt.show()
--- des_sim_dante.py
+++ des_sim_dante.py
@@ -6,11 +6,14 @@ import random
 import simpy
 import pandas as pd
 import numpy as np
-
+import time
+import matplotlib.pyplot as plt
 
 LAMBDA = 10.0  # Generate new customers roughly every lambda seconds
 MU = 9 # Expected waiting time per customer in seconds
-N_CUSTOMERS = 1000
+N_CUSTOMERS = 3000
+N_servers = [1,2,4]
+N_sim = 1000
 
 class System(object):
     """Class for one server queue system"""
@@ -18,6 +21,7 @@ class System(object):
         self.env = env
         self.server = simpy.Resource(env, capacity=n_server)
         self.waittime = 0
+        self.waitlist = []
         self.sojourn = 0
         self.total_cust = 0
         self.mu = mu
@@ -30,46 +34,86 @@ def customer(env, system):
     with system.server.request() as req:
         yield req 
 
-        wait = env.now - arrive
-        system.waittime += wait
-
         # Time in system
         tis = random.expovariate(1/system.mu)
         yield env.timeout(tis)
         
-        sojourn = env.now - arrive
-        system.sojourn += sojourn
+        # Sojourn time, real waiting time
+        wait = env.now - arrive
+
+        # Append only steady state values of waiting time > x customers
+        if system.total_cust > 500:
+            system.waittime += wait
+        system.waitlist.append(wait)
 
 def setup(env,system):
     """Adding customers for one system simulations"""
-    for c in range(system.n_cust):
+    for _ in range(system.n_cust):
         system.total_cust += 1
         env.process(customer(env, system)) # Add customer to process
         t = random.expovariate(1/system.lambd)
         yield env.timeout(t)
 
-N_servers = [1,2,4]
-N_sim = 100
+t0 = time.time()
+
 data_list = [[],[]]
+sum_waiting = [[],[],[]]
 
-# For loop to run all simulations
+# Running all simulations for every n_server
 for i in range(len(N_servers)):
+    print("Server", i)
     for n_sim in range(N_sim):
-
-        # Setup and start the simulation
+        # Choose random random seed
         RANDOM_SEED = random.randint(1,700)
         random.seed(RANDOM_SEED)
+
+        # Setup and start the simulation
         env = simpy.Environment()
         system = System(env, N_servers[i], N_CUSTOMERS, MU, LAMBDA/N_servers[i])
 
         env.process(setup(env, system))
         env.run()
 
+        # Filling large data_list with mean waiting time per simulation
+        # will be used for csv (Could be optimized)
         data_list[0].append(i)
         data_list[1].append(system.waittime/system.total_cust)
 
+        # if n_sim == 0:
+            # # Filling datalist with waiting time per customer for first sim
+            # single_data = {"Waiting": system.waitlist}
+            # df2 = pd.DataFrame(single_data)
+            # df2.to_csv("test_server_"+str(i)+".csv")
+        
+        # Collecting all customer waiting times to determine steady state
+        if n_sim == 0:
+            # Adding first list with waiting times per customer
+            sum_waiting[i] = system.waitlist 
+        else:
+            # Continue adding sum_waiting list
+            before = sum_waiting[i]
+            sum_waiting[i] = np.add(before, system.waitlist)
+            
+    print("Finished all", N_sim, "simulations")
+
+# Creating a csv with the mean waiting time per customer for all servers
+mean_waiting_pc = []
+serverlist = []
+for i in range(len(N_servers)):
+    sublist = list(np.array(sum_waiting[i])/N_sim)
+    mean_waiting_pc.extend(sublist)
+    for c in range(N_CUSTOMERS):
+        serverlist.append(i)
+
+sum_data = {"Server":serverlist, "Waiting pc": mean_waiting_pc}
+df3 = pd.DataFrame(sum_data)
+df3.to_csv("waiting_pc.csv")
+
+# Data file with all mean waiting times per simulation
 data = {"Server": data_list[0], "Mean Wait": data_list[1]}
 df = pd.DataFrame(data)
 df.to_csv("data.csv")
 
-print("Done!")
+t1 = time.time()
+print("\nDid", N_sim, "simulations with", N_CUSTOMERS, "customers, for server", N_servers, "in total time of ", round(t1-t0,3), "seconds\n")
+
