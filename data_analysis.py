a78730fe9e68645ced2c7413a37b3459d9045335
DCCdelang <dccdelang@gmail.com>
2020-11-28 13:23:22 +0100

    additions

--- data_analysis.py
+++ data_analysis.py
@@ -17,6 +17,7 @@ boxplot_wait()
 for n in range(3):
     df2 = pd.read_csv("data.csv")
     serverdata = df2.loc[df2["Server"] == n, "Mean Wait"]
+    print("Shape of waitingdata per server is:", serverdata.shape)
     sns.distplot(serverdata, bins= 20, rug=True)
     plt.show()
 
@@ -28,9 +29,12 @@ for i in range(3):
     plt.show()
 
 # %%
-# Plot to determine when the steady state starts
+# Creates three plots with the mean waiting time per customer number
+# useful to determine when the steady state starts
 for n in range(3):
     df4 = pd.read_csv("waiting_pc.csv")
     waitingdata = df4.loc[df4["Server"] == n, "Waiting pc"]
+    print("Shape of waitingdata per server is:", waitingdata.shape)
     sns.lineplot(data=waitingdata)
     plt.show()
+# %%
